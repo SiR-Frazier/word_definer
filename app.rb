624d2d56f009a8dde81f55d08570f0e9fd92f8a8
Anna Marie Cooper and Bryce Frazier <anna@>
2018-02-12 16:33:17 -0800

    update template

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/parcels')
 
-describe('#_____') do
-  it ("_____") do
-    expect(____()).to(eq("_____"))
-  end
+get('/') do
+  @description = "___"
+  erb(:input)
+end
+
+get('/output') do
+  erb(:output)
+end
